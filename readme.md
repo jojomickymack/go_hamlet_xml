a4a154e6b95d562d59b833c7fd5083f6f8807a3e
jojo <jojomickymack@gmail.com>
2018-04-28 14:31:10 -0600

    reading xml with go

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Parse XML With Go
+
+You can parse XML with the standard Go library. This is an example of how that's done using an xml version of shakespeare's hamlet, which was aquired 
+from [here](https://github.com/severdia/PlayShakespeare.com-XML).
+
+I intended to use this as an exersize for sorting - but realized that when the lines are taken out of context the play is meaningless. That's why I'm 
+reading all of the lines in order, maybe I'll find ways to sort them that retain the context of the lines.
+
+This example is probably helpful for anyone trying to parse xml with go.
---
+++ src/hamlet/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Play struct {
+	Title      string       `xml:"title"`
+	Playwright string       `xml:"playwright"`
+	Personae   PersonaeType `xml:"personae"`
+	Acts       []Act        `xml:"act"`
+}
+
+type PersonaeType struct {
+	Cast []PersonaType `xml:"persona"`
+}
+
+type PersonaType struct {
+	Persname PersnameType `xml:"persname"`
+}
+
+type PersnameType struct {
+	Name  string `xml:",chardata"`
+	Short string `xml:"short,attr"`
+}
+
+type Act struct {
+	Acttitle string      `xml:"acttitle"`
+	Scenes   []SceneType `xml:"scene"`
+}
+
+type SceneType struct {
+	Scenetitle    string       `xml:"scenetitle"`
+	Scenelocation string       `xml:"scenelocation"`
+	Scenepersonae []string     `xml:"scenepersonae"`
+	Speeches      []SpeechType `xml:"speech"`
+}
+
+type SpeechType struct {
+	Speaker string   `xml:"speaker"`
+	Lines   []string `xml:"line"`
+}
+
+func (p Play) printPlayDetails() {
+	fmt.Printf("the play is %s\n", p.Title)
+}
+
+func main() {
+	xmlFile, err := os.Open("hamlet.xml")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer xmlFile.Close()
+
+	data, _ := ioutil.ReadAll(xmlFile)
+
+	var doc Play
+	xml.Unmarshal(data, &doc)
+
+	fmt.Printf("%s\n", doc.Title)
+	fmt.Printf("%s\n", doc.Playwright)
+
+	println("\nCast\n")
+
+	for _, persona := range doc.Personae.Cast {
+		fmt.Printf("%-20s\t%s\n", persona.Persname.Short, persona.Persname.Name)
+	}
+
+	println("\n")
+
+	for _, act := range doc.Acts {
+		println(act.Acttitle + "\n")
+		for _, scene := range act.Scenes {
+			for _, speech := range scene.Speeches {
+				println(speech.Speaker)
+				for _, line := range speech.Lines {
+					println(line)
+				}
+				println("\n")
+			}
+			println("\n")
+		}
+		println("\n\n")
+	}
+}
